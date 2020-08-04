623ddd62a6b30c8639af6d00782428f3c6c28e75
AndyIsraelYanez <camilo.casas200@gmail.com>
2020-08-04 12:48:32 -0500

    Cambios

---
+++ ConsolaRepaso/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsolaRepaso
+{
+    class Program
+    {
+       static void Main(string[] args)
+
+        {
+            Console.WriteLine("--------------");
+            Console.WriteLine("** Presione Enter para continuar **");
+            Console.ReadKey();
+            Console.WriteLine("¿Ingrese su nombre para acceder a la información?");
+            string nombreUsuario = Console.ReadLine();
+
+            Vendedor vendedor = new Vendedor
+            {
+                Nombre = "Andy",
+                Nacimiento = new DateTime(2000, 4, 14)
+
+            };
+
+            Console.WriteLine(vendedor.Saludar());
+            Console.WriteLine(vendedor.Cobrar());
+            Console.WriteLine(vendedor.DarVueltos());
+
+            Console.WriteLine($"Muchas gracias  {nombreUsuario} por visitarnos");
+        }
+    }
+}
---
+++ ConsolaRepaso/Vendedor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ConsolaRepaso
+{
+    public class Vendedor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Nacimiento { get; set; }
+        [NotMapped]
+        public int EdadAños
+        {
+            get
+            {
+                return DateTime.Now.Year - Nacimiento.Year;
+            }
+        }
+        public string CiudadDondeVive { get; set; }
+        public string Saludar()
+        {
+            return $" Hola soy {Nombre} y soy un vendedor  de artefactos";
+        }
+        public string Cobrar()
+        {
+            return $" Si a una persona le gusta el producto yo soy el que cobra ";
+        }
+        public string DarVueltos()
+        {
+            return $" y ademas  soy el que da los vueltos. ";
+        }
+
+    }
+}
